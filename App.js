d56f57c33ec364d35e5410fbabd5071b786272ec
Daiana89 <daianachaves_89@hotmail.com>
2024-07-12 11:12:10 -0300

    Creacion de botones y logo

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TouchableHighlight } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+        <image source={{uri:'./assets/logo.png'}}/>
+        <Text style={styles.text}>Hola</Text>
+        <StatusBar style="auto" />
+        <TouchableHighlight style={styles.TouchableHighlight}>
+          <Text style={styles.textBotton}>VERDAD O RETO</Text>
+        </TouchableHighlight> 
+        <TouchableHighlight style={styles.TouchableHighlight}>
+          <Text style={styles.textBotton}>REGLAS</Text>
+        </TouchableHighlight> 
+        
     </View>
   );
 }
@@ -13,8 +23,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'black',
+    fontSize: 42,
+    lineHeight: 84,
+    textAlign: 'center',
+   
+  },
+  TouchableHighlight:{
+    // underlayColor:'black',
+    borderWidth: 3,
+    borderColor: "black",
+    borderRadius: 40,
+    width:250, 
+    height:50,
+    backgroundColor:'white',
+    marginBottom:20,
+
+  },
+  textBotton:{
+    fontSize: 18,
+    textAlign: 'center',
+    paddingVertical: 10,
+    fontWeight: 'bold',
+    
+
+  }
+
 });
